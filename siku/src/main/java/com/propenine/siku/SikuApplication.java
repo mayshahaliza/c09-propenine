6a01502ada7b51a9343280c88bc4e235a2239631
maysha.haliza <maysha.haliza@ui.ac.id>
2024-02-17 20:41:49 +0700

    initialize

---
+++ siku/src/main/java/com/propenine/siku/SikuApplication.java
@@ -0,0 +1,13 @@
+package com.propenine.siku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikuApplication.class, args);
+	}
+
+}
